7aa0dd06bd3268a551ad48adf1bff8c4c25d8a69
Kenrl3301 <kenier.rochal@uqvirtual.edu.co>
2026-04-16 20:26:52 -0500

    Se agrego el costructor y las relaciones de la clase Destinatario

--- Envio/src/main/java/uniquindio/edu/co/Destinatario.java
+++ Envio/src/main/java/uniquindio/edu/co/Destinatario.java
@@ -1,4 +1,22 @@
 package uniquindio.edu.co;
 
 public class Destinatario {
+    private String nombre;
+    private String documento;
+    private String telefono;
+    private String ciudad;
+
+    public Envio theEnvioDestinatario;
+    public Empresa theEmpresaDestinatario;
+
+    public Destinatario(nombre, documento, telefono, ciudad){
+        this.nombre = nombre;
+        this.documento = documento;
+        this.telefono = telefono;
+        this.ciudad = ciudad;
+   }
+
+   public InformeDestinatario generarInformeDestinatario(){
+        return new InformeDestinatario (nombre, documento, telefono, ciudad);
+   }
 }
